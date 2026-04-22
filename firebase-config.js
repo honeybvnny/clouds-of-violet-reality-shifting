d47cf7700c4c88bbbec026c88b91c923ebb71965
honeybvnny <103810664+honeybvnny@users.noreply.github.com>
2026-04-22 04:47:56 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
 export const firebaseProject = {
-  enabled: false,
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  enabled: true,
+  apiKey: "AIzaSyBfrxlgSwBBBnFuowGbrkcVpETx3utMy7g",
+  authDomain: "cloudnova-c547b.firebaseapp.com",
+  projectId: "cloudnova-c547b",
+  storageBucket: "cloudnova-c547b.firebasestorage.app",
+  messagingSenderId: "63202634600",
+  appId: "1:63202634600:web:14bae57ca4279f876f44dd"
+  measurementId: "G-GV1WSEEZ96
 };
