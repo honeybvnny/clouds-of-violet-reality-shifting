ce70288f8f6e1b3636cce80f19d443f3c27621ff
honeybvnny <103810664+honeybvnny@users.noreply.github.com>
2026-04-22 03:39:06 -0400

    Create firebase-config.example.js

---
+++ firebase-config.example.js
@@ -0,0 +1,9 @@
+export const firebaseProject = {
+  enabled: false,
+  apiKey: "your-api-key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
